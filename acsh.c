e3f04bcba4fdb6dc948a62e9362485faaad2a99f
Acid Weaver <acid.weaver@gmail.com>
2024-09-09 17:26:06 +0300

    add pathfinder

--- acsh.c
+++ acsh.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+// #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 static void sig_int(int);
+char *get_command_path(char *file_name);
 
 /**
  * main - Main entry point for our program
@@ -18,7 +21,7 @@ static void sig_int(int);
 int main(int argc, char **argv) {
 
   (void)argc, (void)argv;
-  char *command_str = NULL;
+  char *command_path, *command_str = NULL;
   size_t command_str_allocated = 0;
   ssize_t command_len;
 
@@ -38,8 +41,8 @@ int main(int argc, char **argv) {
 
     // INPUT
     command_len = getline(&command_str, &command_str_allocated, stdin);
-    if (command_str[command_len - 1] == '\n')
-      command_str[command_len - 1] = 0;
+    // if (command_str[command_len - 1] == '\n')
+    // command_str[command_len - 1] = 0;
 
     token = strtok(command_str, " \n");
     arguments = malloc(sizeof(char *) * 1024);
@@ -51,6 +54,8 @@ int main(int argc, char **argv) {
     }
     arguments[i] = NULL;
 
+    command_path = get_command_path(arguments[0]);
+
     // EXIT
     if (command_len == -1) { // EOF case
       break;
@@ -61,6 +66,9 @@ int main(int argc, char **argv) {
       err_sys("error to fork process!");
     } else if (pid == 0) {
 
+      if (execve(command_path, arguments, NULL) == -1) {
+        err_sys("Error to execute command");
+      }
       printf("-----|-----|-----\n"
              "NEW CHILD PROCESS\n"
              "\tPID: %lu\n"
@@ -69,9 +77,6 @@ int main(int argc, char **argv) {
              "\tcommand_str_allocated: %lu\n"
              "-----|-----|-----\n",
              (long)getpid(), command_str, command_len, command_str_allocated);
-      if (execve(arguments[0], arguments, NULL) == -1) {
-        err_sys("Error to execute command");
-      }
       exit(0);
 
     } else if ((pid = waitpid(pid, &status, 0)) < 0)
@@ -82,6 +87,49 @@ int main(int argc, char **argv) {
   free(command_str);
   write(STDOUT_FILENO, "\n-----|-----|-----|-----|-----|-----|-----", 42);
   return (0);
+}
+
+char *get_command_path(char *file_name) {
+  char *path = getenv("PATH");
+  char *token;
+  struct stat file_path;
+  char *path_buffer = NULL;
+
+  if (!path) {
+    perror("PATH not found");
+  }
+
+  token = strtok(path, ":");
+
+  while (token) {
+    if (path_buffer) {
+      free(path_buffer);
+      path_buffer = NULL;
+    }
+    path_buffer = malloc(sizeof(token) + sizeof(file_name) + sizeof(char) * 2);
+    if (!path_buffer) {
+      perror("Error: malloc failed");
+      exit(EXIT_FAILURE);
+    }
+    strcpy(path_buffer, token);
+    strcat(path_buffer, "/");
+    strcat(path_buffer, file_name);
+    strcat(path_buffer, "\0");
+
+    if (stat(path_buffer, &file_path) == 0 && access(path_buffer, X_OK) == 0) {
+      // free(path);
+      return (path_buffer);
+    }
+    token = strtok(NULL, ":");
+  }
+
+  // FINALLY
+  // free(path);
+  if (path_buffer)
+    free(path_buffer);
+
+  perror("Absolute path not found");
+  return (NULL);
 }
 
 void sig_int(int signo) { printf("Interruption!\n"); }
