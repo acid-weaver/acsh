ad2bcc93d947e1289cde70f23590199f74167ce6
Acid Weaver <acid.weaver@gmail.com>
2024-09-09 12:25:25 +0300

    minor improovements

--- acsh.c
+++ acsh.c
@@ -1,7 +1,11 @@
-// #include "acsh.h"
+#include "apue.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
+
+static void sig_int(int);
 
 /**
  * main - Main entry point for our program
@@ -14,27 +18,53 @@
 int main(int argc, char **argv) {
 
   (void)argc, (void)argv;
-  char *command = NULL;
-  size_t command_len = 0;
-  ssize_t output;
+  char *command_str = NULL;
+  size_t command_str_allocated = 0;
+  ssize_t command_len;
 
+  pid_t pid;
+  int status;
+
+  // SIGNALS INIT
+  if (signal(SIGINT, sig_int) == SIG_ERR)
+    err_sys("Error to call signal.");
+
+  // REPL
   do {
     write(STDOUT_FILENO, "$ ", 2);
 
     // INPUT
-    output = getline(&command, &command_len, stdin);
+    command_len = getline(&command_str, &command_str_allocated, stdin);
+    if (command_str[command_len - 1] == '\n')
+      command_str[command_len - 1] = 0;
 
     // EXIT
-    if (output == -1) { // EOF case
+    if (command_len == -1) { // EOF case
       break;
     }
 
-    // DEBUG
-    printf("DEBUG MESSAGE.\n\tline=%s\n", command);
+    // RUN
+    if ((pid = fork()) < 0) {
+      err_sys("error to fork process!");
+    } else if (pid == 0) {
+
+      printf("-----|-----|-----\n"
+             "NEW PROCESS\n"
+             "\tPID: %lu\n"
+             "\tcommand_str: %s\n"
+             "\tcommand_str_allocated: %lu\n"
+             "-----|-----|-----\n",
+             (long)getpid(), command_str, command_str_allocated);
+      exit(0);
+
+    } else if ((pid = waitpid(pid, &status, 0)) < 0)
+      err_sys("error to call waitpid");
   } while (1);
 
   // FINALLY
-  free(command);
+  free(command_str);
   write(STDOUT_FILENO, "\n-----|-----|-----|-----|-----|-----|-----", 42);
   return (0);
 }
+
+void sig_int(int signo) { printf("Interruption!\n"); }
--- acsh.h
+++
@@ -1,9 +0,0 @@
-#ifndef ACSH_H
-#define ACSH_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#endif /* ACSH_H */
-
